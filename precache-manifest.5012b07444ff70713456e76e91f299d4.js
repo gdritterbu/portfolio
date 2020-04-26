cde9606b809257c5b8b4bdae7469eeeb30f62d4a
gdritterbu <ritterbusch@th-brandenburg.de>
2020-04-26 21:28:13 +0200

    Updates

---
+++ precache-manifest.5012b07444ff70713456e76e91f299d4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daa1a6cffefc92e13a49dc4d9f596509",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "227a5420b818851b7868",
+    "url": "/portfolio/static/css/main.6edeb043.chunk.css"
+  },
+  {
+    "revision": "8557e222d98df880b501",
+    "url": "/portfolio/static/js/2.528c1d6f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.528c1d6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "227a5420b818851b7868",
+    "url": "/portfolio/static/js/main.d8d5da59.chunk.js"
+  },
+  {
+    "revision": "ee3d72d161f98a5d4b9d",
+    "url": "/portfolio/static/js/runtime-main.f18b6ef5.js"
+  },
+  {
+    "revision": "eb4b031fd432e653e4f7db388d6f196f",
+    "url": "/portfolio/static/media/background.eb4b031f.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e38b86da464732056c567e9ed28f80f8",
+    "url": "/portfolio/static/media/video.e38b86da.mp4"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f9ef4fefce0d41736710b6df28dc9009.js"
+  "/portfolio/precache-manifest.5012b07444ff70713456e76e91f299d4.js"
 );
 
 self.addEventListener('message', (event) => {
